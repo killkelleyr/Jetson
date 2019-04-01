6c05cafefcd000ee882df02ff00a4dfcc34e93a8
robert killkelley <robertkillkelley@gmail.com>
2019-03-31 21:06:17 -0400

    client side test code
    this code takes in data in the form [x,y] and separates it as two separate variables x and y

--- testCode/client.py
+++ testCode/client.py
@@ -1,35 +1,25 @@
 import time
-from multiprocessing import Process, Manager, Value
 import socket
 
 HOST = '192.168.1.198'
 PORT = 5002
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-sep = ' '
 
+data = ""
 def get_controller():
-	data = 2554
-	print("Got to loop")
 	while True:
 		buf = ''
 		while len(buf) < 9:
-			buf += s.recv(8)
-		
-		try:
-			left,right=buf.split(",")
-			print("Left: "+left,right)
-			dataL = int(left)
-			dataR = int(right)
-			if(len(str(dataL)) > 3):
-				set_esc(dataL)
-			if(len(str(dataR)) > 3):
-				set_esc(dataR)
-				#print("ESC Value: "+str(dataL)+","+str(dataR))
-		except:
-			data = data
-
-def set_esc(val):
-	print("Received: "+str(val))
+			hold += s.recv(1)
+			if hold == "[":
+				pass
+			elif hold == "]":
+				pass
+			else
+				buf += hold
+		left,right = buf.split(',')
+		print("L: "+str(left)+" R: "+str(right))
+		hold = ''
 
 get_controller()
